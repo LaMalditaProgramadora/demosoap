a776874ab6920904ad185474479dcf76ed6474c7
Jimena Alexandra Ruiz Cerna <jruiz@novatronic.com>
2023-05-03 22:42:19 -0500

    Fix dependencias Spring Boot 2

--- src/main/java/com/example/demosoap/gen/GetProductsResponse.java
+++ src/main/java/com/example/demosoap/gen/GetProductsResponse.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
 // Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2023.02.24 a las 05:17:59 PM COT 
+// Generado el: 2023.02.24 a las 05:17:59 PM COT
 //
 
 
@@ -19,9 +19,9 @@ import javax.xml.bind.annotation.XmlType;
 
 /**
  * <p>Clase Java para anonymous complex type.
- * 
+ *
  * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
- * 
+ *
  * <pre>
  * &lt;complexType>
  *   &lt;complexContent>
@@ -33,12 +33,12 @@ import javax.xml.bind.annotation.XmlType;
  *   &lt;/complexContent>
  * &lt;/complexType>
  * </pre>
- * 
- * 
+ *
+ *
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "", propOrder = {
-    "products"
+        "products"
 })
 @XmlRootElement(name = "getProductsResponse")
 public class GetProductsResponse {
@@ -48,25 +48,25 @@ public class GetProductsResponse {
 
     /**
      * Gets the value of the products property.
-     * 
+     *
      * <p>
      * This accessor method returns a reference to the live list,
      * not a snapshot. Therefore any modification you make to the
      * returned list will be present inside the JAXB object.
      * This is why there is not a <CODE>set</CODE> method for the products property.
-     * 
+     *
      * <p>
      * For example, to add a new item, do as follows:
      * <pre>
      *    getProducts().add(newItem);
      * </pre>
-     * 
-     * 
+     *
+     *
      * <p>
      * Objects of the following type(s) are allowed in the list
      * {@link Product }
-     * 
-     * 
+     *
+     *
      */
     public List<Product> getProducts() {
         if (products == null) {
@@ -74,7 +74,7 @@ public class GetProductsResponse {
         }
         return this.products;
     }
-    
+
     public void setProducts(List<Product> values) {
         this.products = values;
     }
--- src/main/java/com/example/demosoap/gen/Product.java
+++ src/main/java/com/example/demosoap/gen/Product.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
 // Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2023.02.24 a las 05:17:59 PM COT 
+// Generado el: 2023.02.24 a las 05:17:59 PM COT
 //
 package com.example.demosoap.gen;
 
@@ -42,10 +42,10 @@ import javax.xml.bind.annotation.XmlType;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "product", propOrder = {
-    "id",
-    "name",
-    "price",
-    "description"
+        "id",
+        "name",
+        "price",
+        "description"
 })
 @Entity
 public class Product {
