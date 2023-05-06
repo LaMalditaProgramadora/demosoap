b09f4c97a669170112cb8b20a9e109fead8a1636
Jimena Alexandra Ruiz Cerna <jruiz@novatronic.com>
2023-05-05 23:52:18 -0500

    Update ProductRepository.java

--- src/main/java/com/example/demosoap/repository/ProductRepository.java
+++ src/main/java/com/example/demosoap/repository/ProductRepository.java
@@ -3,7 +3,9 @@ package com.example.demosoap.repository;
 import com.example.demosoap.gen.Product;
 import com.example.demosoap.model.ProductModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<ProductModel, Integer> {
     Product findByName(String name);
 }
